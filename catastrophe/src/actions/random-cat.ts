e589d891d944c6fdca5a8f659a24732e98d9f3bc
Zack Hoherchak <zack.hoherchak@corsair.com>
2024-09-03 13:42:52 -0400

    add jsdocs

--- catastrophe/src/actions/random-cat.ts
+++ catastrophe/src/actions/random-cat.ts
@@ -18,6 +18,9 @@ export class RandomCat extends SingletonAction<RandomCatSettings> {
 	private visibleAutoActions = new Map<String, Action>();
 	private timer: NodeJS.Timeout | null = null;
 
+	/**
+	 * Sets the initial action image, stores the action for auto-updating, and establishes a timer for auto-updating.
+	 */
 	onWillAppear(ev: WillAppearEvent<RandomCatSettings>): void {
 		// Set a random cat image when the key appears.
 		this.setRandomCat(ev.action);
@@ -36,6 +39,9 @@ export class RandomCat extends SingletonAction<RandomCatSettings> {
 		}
 	}
 
+	/**
+	 * Removes the action from the auto-update list (as it is no longer visible) and clears the timer if no more actions are auto-updating.
+	 */
 	onWillDisappear(ev: WillDisappearEvent<RandomCatSettings>): void {
 		this.visibleAutoActions.delete(ev.action.id);
 
@@ -46,6 +52,9 @@ export class RandomCat extends SingletonAction<RandomCatSettings> {
 		}
 	}
 
+	/**
+	 * Adds or removes the action from the auto-update list based on the settings provided by the property inspector.
+	 */
 	onDidReceiveSettings(ev: DidReceiveSettingsEvent<RandomCatSettings>): Promise<void> | void {
 		if (ev.payload.settings.autoUpdate) {
 			this.visibleAutoActions.set(ev.action.id, ev.action);
@@ -67,10 +76,17 @@ export class RandomCat extends SingletonAction<RandomCatSettings> {
 		}
 	}
 
+	/**
+	 * Sets a new random cat image when the key is pressed.
+	 */
 	onKeyUp(ev: KeyUpEvent<RandomCatSettings>): void | Promise<void> {
 		this.setRandomCat(ev.action);
 	}
 
+	/**
+	 * Fetches a random cat image and sets it as the action's image.
+	 * @param action The action in which to apply the new cat image.
+	 */
 	private async setRandomCat(action: Action) {
 		try {
 			const response = await fetch('https://cataas.com/cat?width=144&height=144');
