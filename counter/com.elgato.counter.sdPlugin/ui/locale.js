8cc82bad8716c000bacc131285ffcf6805dafff9
Zack Hoherchak <zhoherchak@gmail.com>
2026-05-26 12:00:41 -0400

    Counter disable multi action, Add localization (#15)
    * remove multi action support
    * add localization

---
+++ counter/com.elgato.counter.sdPlugin/ui/locale.js
@@ -0,0 +1,26 @@
+SDPIComponents.i18n.locales = {
+	en: {
+		incrementBy: "Increment By",
+	},
+	de: {
+		incrementBy: "Erhöhen um",
+	},
+	es: {
+		incrementBy: "Incrementar en",
+	},
+	fr: {
+		incrementBy: "Incrémenter de",
+	},
+	ja: {
+		incrementBy: "増分",
+	},
+	ko: {
+		incrementBy: "증가량",
+	},
+	pt: {
+		incrementBy: "Incrementar em",
+	},
+	zh_CN: {
+		incrementBy: "递增值",
+	},
+};
